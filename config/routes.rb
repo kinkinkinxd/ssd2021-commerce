b98841ccf7d9a973717444d4c160712fcb71a89e
kinkinkinxd <kinkinkinxd@gmail.com>
2021-05-14 22:50:04 +0700

    Create order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin
   resources :categories
+  get '/orders/new_order', to: 'orders#new', as: 'orders_new'
+  resources :orders
   namespace :admins do
     resources :products
     namespace :products do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_070600) do
+ActiveRecord::Schema.define(version: 2021_05_14_151136) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,12 +61,13 @@ ActiveRecord::Schema.define(version: 2021_04_12_070600) do
   create_table "categories_products", id: false, force: :cascade do |t|
     t.integer "category_id", null: false
     t.integer "product_id", null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
+    t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
   end
 
-  create_table "commerces", force: :cascade do |t|
-    t.text "title"
-    t.text "description"
-    t.integer "stock"
+  create_table "orders", force: :cascade do |t|
+    t.integer "product_id"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -78,18 +79,6 @@ ActiveRecord::Schema.define(version: 2021_04_12_070600) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
